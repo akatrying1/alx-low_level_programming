6e08911dbae165fccbc8f815644e12d3f966968b
akatrying1 <omarhathout2@gmail.com>
2023-07-21 05:30:59 -0700

    ronaldinho

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * print_alphabet
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int(ch);
+
+for (ch = 'a'; ch <= 'Z'; ch++)
+putchar (ch);
+putchar ('/n');
+}
